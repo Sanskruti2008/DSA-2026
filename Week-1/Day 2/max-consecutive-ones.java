a5ed1cd732170c767cbf8386d2cc6dbdb06e4d2c
Sanskruti Tushar Suryawanshi <101386900+Sanskruti2008@users.noreply.github.com>
2026-09-02 21:38:09 +0530

    Implement solution for max consecutive ones problem

---
+++ Week-1/Day 2/max-consecutive-ones.java
@@ -0,0 +1,20 @@
+/*https://leetcode.com/problems/max-consecutive-ones/*/
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+
+        int current = 0;
+        int max = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+
+            if(nums[i] == 1) {
+                current++;
+                max = Math.max(max, current);
+            } else {
+                current = 0;
+            }
+        }
+
+        return max;
+    }
+}
