1e96541d15e7e1bce0e7306fba8857aa37b94fb9
Sanskruti Tushar Suryawanshi <101386900+Sanskruti2008@users.noreply.github.com>
2026-09-05 19:26:46 +0530

    Add pivot index finder in find-pivot-index.java
    Implement solution to find the pivot index in an array.

---
+++ Week-1/Day-4/find-pivot-index.java
@@ -0,0 +1,24 @@
+/*https://leetcode.com/problems/find-pivot-index/*/
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum=0;
+        
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+        }
+                   int leftsum=0;
+
+         for(int i=0;i<nums.length;i++){
+           int rightsum=sum-leftsum-nums[i];
+
+           if(leftsum == rightsum){
+            return i;
+           }
+           leftsum += nums[i];
+           
+        }
+        return -1;
+    }
+}        
+    
